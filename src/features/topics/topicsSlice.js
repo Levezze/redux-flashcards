892f3ac87971a67b3f9ae475b48cb43c59a98e65
Levezze <lev.zhi@gmail.com>
2025-01-06 01:28:20 -0500

    Add redux slices for topics and quizzes

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   topics: {}
-}
+};
 
 export const topicsSlice = createSlice({
   name: 'topics',
@@ -10,11 +10,16 @@ export const topicsSlice = createSlice({
   reducers: {
     addTopic(state, action) {
       const { id, name, icon } = action.payload;
-      state[id] = { id, name, icon, quizIds: [] }
+      state.topics[id] = { id, name, icon, quizIds: [] }
+    },
+    addQuizId(state, action) {
+      const { id, topicId } = action.payload;
+      console.log('test', state.topics[topicId]);
+      state.topics[topicId].quizIds.push(id);
     }
   }
 });
 
 export const selectTopics = (state) => state.topics.topics;
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
